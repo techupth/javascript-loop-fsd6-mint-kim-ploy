c1561e2a658e2a2781e2f0e75e27aed9b87012da
PNKim <paljapon.nait@gmail.com>
2024-05-16 12:50:51 +0700

    feat: resolve ex1-6

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,12 @@
 // Exercise #3: Find a Minimum Score
 let scores = [100, 20, 3, 1000];
-let minScore;
+let minScore = scores[0];
 // Start coding here
+// minScore = Math.min(...scores);
+for (let num of scores) {
+  if (minScore > num) {
+    minScore = num;
+  }
+}
 
 console.log(minScore);
--- ex-4.js
+++ ex-4.js
@@ -2,5 +2,9 @@
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
 // Start coding here
+for (let name of restaurants) {
+  let text = "Restaurant Name: ";
+  newRestaurants.push(text + name);
+}
 
 console.log(newRestaurants);
